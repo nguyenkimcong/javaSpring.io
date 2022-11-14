7660acd7ceccfd5190fc056e902d405cdc644237
git <nguyenkimcong1999@gmail.com>
2022-11-14 16:55:00 +0700

    baitap

--- day_02/firstweb/src/main/java/com/example/firstweb/exception/NotFoundExcepiton.java
+++
@@ -1,8 +0,0 @@
-package com.example.firstweb.exception;
-
-public class NotFoundExcepiton extends  RuntimeException {
-    public NotFoundExcepiton(String message) {
-        super(message);
-
-    }
-}
